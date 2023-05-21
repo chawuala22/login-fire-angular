9ec8b938c61f8855e18021ff3c02abf483e78cbd
chawuala <mrojas@econtainers.co>
2023-05-21 17:35:15 -0500

    Integracion de angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { AppRoutingModule } from './app-routing.module';
 
 
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -24,7 +28,10 @@ import { AppComponent } from './app.component';
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'login-fire-angular-6de9c',
+    appId: '1:1085046286527:web:b1211c061c351c2eaafe0a',
+    storageBucket: 'login-fire-angular-6de9c.appspot.com',
+    apiKey: 'AIzaSyDbMd8Qk9D7OS8CSUzB_oA7jmm0jAq-q3k',
+    authDomain: 'login-fire-angular-6de9c.firebaseapp.com',
+    messagingSenderId: '1085046286527',
+  },};
